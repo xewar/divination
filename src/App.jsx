4d387d750669fabce1338c3091e7cea7e4d197f4
Xewar <xewardreams@gmail.com>
2022-10-27 22:04:45 -0400

    setting up project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Canvas from './Canvas';
+function App() {
+  return (
+    <div className="App">
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
